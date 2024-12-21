5977a0140206b39da79ff6660bc50e1538c9e5b5
method78 <lashkomaksim19@gmail.com>
2024-12-21 17:49:15 +0300

    v1.1 (нло двигается и стреляет)

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import pygame
 import sys
 import math
+from random import randint
 
 pygame.init()
 
-width, height = 501, 501
+width, height = 1000, 500
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("PyWars_v1.0")
 
 background_color = (0, 0, 0)
 
+main_bullet_image = pygame.image.load("images/main_bullet.png")
 warrior_image = pygame.image.load("images/warrior.png")
-warrior_rect = warrior_image.get_rect(center=(width // 2, height // 2))
 
-speed = 5
+warrior_rect = warrior_image.get_rect(center=(50, 250))
+list_enemies = []
+for _ in range(2):
+    for _ in range(10):
+        pass  # создание врагов (пушек)
+
+speed = 10
 target = None
 
 
@@ -32,9 +39,14 @@ def move_towards_target(rect, target_x, target_y):
 
 
 def main():
-    global warrior_rect, target
+    global warrior_rect, target, main_bullet_image
     clock = pygame.time.Clock()
-
+    MYEVENTTYPE = pygame.USEREVENT + 1  # событие движения пуль
+    pygame.time.set_timer(MYEVENTTYPE, 1)
+    MYEVENTTYPE2 = pygame.USEREVENT + 1  # событие выстрела врагов
+    pygame.time.set_timer(MYEVENTTYPE2, 50)
+    list_main_bullets = []  # список наших пуль
+    list_enemy_bullets = []  # список вражеских пуль
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -42,15 +54,29 @@ def main():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 target = event.pos
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:  # действие на пробел
+                    list_main_bullets.append(main_bullet_image.get_rect(center=(warrior_rect.centerx + 30,
+                                                                                warrior_rect.centery + 10)))
+            if event.type == MYEVENTTYPE:
+                for bullet in list_main_bullets:  # цикл движения наших пуль
+                    bullet.centerx += 10
+            if event.type == MYEVENTTYPE2:
+                pass  # выстрел врагов каждые пол секунды
 
         screen.fill(background_color)
-
         if target:
-            warrior_rect = move_towards_target(warrior_rect, target[0], target[1])
+            if target[0] > 800:
+                warrior_rect = move_towards_target(warrior_rect, 800, target[1])
+            else:
+                warrior_rect = move_towards_target(warrior_rect, target[0], target[1])
             if math.hypot(target[0] - warrior_rect.centerx, target[1] - warrior_rect.centery) < speed:
                 target = None
 
         screen.blit(warrior_image, warrior_rect)
+        if list_main_bullets:
+            for bullet in list_main_bullets:
+                screen.blit(main_bullet_image, bullet)
         pygame.display.flip()
         clock.tick(60)
 
